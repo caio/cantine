09731cee2deaa7d69b37c9ed918b7963fed49053
Caio Romão <hello@caio.co>
2019-07-13 23:04:32 +0200

    Basic in-memory search endpoint
    Still nice. Starting to regret not investing time in rust sooner.

--- crates/tique/src/main.rs
+++ crates/tique/src/main.rs
@@ -1,10 +1,21 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result as ActixResult};
 use serde::{Deserialize, Serialize};
 use std::sync::atomic::{AtomicU16, Ordering};
 use std::sync::Arc;
+use tantivy::collector::TopDocs;
+use tantivy::query::QueryParser;
+use tantivy::schema::{Document, Field, SchemaBuilder, STORED, TEXT};
+use tantivy::ReloadPolicy;
+use tantivy::{Index, IndexReader};
 
 struct AppState {
     counter: Arc<AtomicU16>,
+}
+
+struct SearchState {
+    index_reader: IndexReader,
+    query_parser: QueryParser,
+    id_field: Field,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -14,28 +25,90 @@ struct SearchQuery {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct SearchResult {
-    recipe_ids: Vec<i32>,
+    recipe_ids: Vec<u64>,
 }
 
-fn index(state: web::Data<AppState>) -> impl Responder {
-    let cur = state.counter.fetch_add(1, Ordering::SeqCst) + 1;
+fn index_(state: web::Data<AppState>) -> impl Responder {
+    let cur = state.counter.fetch_add(1, Ordering::Relaxed) + 1; // Monotonic
     format!("Trololo {}!", cur)
 }
 
-fn search(query: web::Json<SearchQuery>) -> Result<HttpResponse> {
-    println!("Searching: {:?}", query.0);
+fn search(
+    state: web::Data<SearchState>,
+    query: web::Json<SearchQuery>,
+) -> ActixResult<HttpResponse> {
+    println!("Searching: {:?}", query);
+
+    let searcher = state.index_reader.searcher();
+    let query = state
+        .query_parser
+        .parse_query(&*query.q)
+        .expect("Failed to parse query");
+
+    let top_docs = searcher
+        .search(&query, &TopDocs::with_limit(10))
+        .expect("Failed to execute search");
+
+    let found_ids = top_docs
+        .iter()
+        .map(|(_, d)| {
+            searcher
+                .doc(*d)
+                .expect("search found doc that doesnt exist?")
+                .get_first(state.id_field)
+                .expect("id field empty?")
+                .u64_value()
+        })
+        .collect();
+
     Ok(HttpResponse::Ok().json(SearchResult {
-        recipe_ids: vec![1, 2, 3, 4, 5],
+        recipe_ids: found_ids,
     }))
 }
 
-fn main() -> std::result::Result<(), std::io::Error> {
+fn init_search() -> SearchState {
+    let mut schema_builder = SchemaBuilder::default();
+
+    let id = schema_builder.add_u64_field("id", STORED);
+    let title = schema_builder.add_text_field("title", TEXT);
+
+    let schema = schema_builder.build();
+    let index = Index::create_in_ram(schema.clone());
+    let mut writer = index.writer(5_000_000).unwrap();
+
+    let make_doc = |i: u64, t: &str| -> Document {
+        let mut doc = Document::new();
+
+        doc.add_u64(id, i);
+        doc.add_text(title, t);
+        doc
+    };
+
+    writer.add_document(make_doc(1, "caio"));
+    writer.add_document(make_doc(2, "caio romao"));
+    writer.add_document(make_doc(3, "caio romao costa nascimento"));
+
+    writer.commit().unwrap();
+
+    SearchState {
+        index_reader: index
+            .reader_builder()
+            .reload_policy(ReloadPolicy::OnCommit)
+            .try_into()
+            .unwrap(),
+        query_parser: QueryParser::for_index(&index, vec![title]),
+        id_field: id,
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
             .data(AppState {
                 counter: Arc::new(AtomicU16::new(0)),
             })
-            .route("/", web::get().to(index))
+            .data(init_search())
+            .route("/", web::get().to(index_))
             .route("/search", web::post().to(search))
     })
     .bind("0.0.0.0:42000")?
