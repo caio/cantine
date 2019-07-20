b278e8948e1393076e6084f9c1ce205fcd0112d7
Caio Romão <hello@caio.co>
2019-07-20 10:20:34 +0200

    Don't regen recipe_generated.rs on main.rs changes

--- crates/tique/build.rs
+++ crates/tique/build.rs
@@ -3,7 +3,6 @@ use std::path::Path;
 
 fn main() {
     println!("cargo:rerun-if-changed=src/recipe.fbs");
-    println!("cargo:rerun-if-changed=src/main.rs");
     flatc_rust::run(flatc_rust::Args {
         inputs: &[Path::new("src/recipe.fbs")],
         out_dir: Path::new("src/database/"),
