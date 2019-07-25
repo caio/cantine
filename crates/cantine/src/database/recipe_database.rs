edc114f0de6142c376df7f2142d9017378ac5ce3
Caio Romão <hello@caio.co>
2019-07-25 21:23:07 +0200

    Use serde + bincode instead of flatbuffers
    Serde does zero-copy reads since 1.0.0, servo's bincode is excellent.
    No more reasons for preferring flatbuffers. <3
    
    TODO Verify the zero copy thing

--- crates/cantine/src/database/recipe_database.rs
+++ crates/cantine/src/database/recipe_database.rs
@@ -2,12 +2,11 @@ use std::collections::HashMap;
 use std::io;
 use std::path::Path;
 
+use bincode::{deserialize, serialize};
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use serde::{Deserialize, Serialize};
 
 use super::mapped_file::AppendOnlyMappedFile;
-
-#[allow(dead_code, unused_imports)]
-use super::recipe_generated::{get_root_as_recipe, Recipe, RecipeArgs};
 
 type Result<T> = super::Result<T>;
 
@@ -15,6 +14,12 @@ pub struct RecipeDatabase {
     log: AppendOnlyMappedFile,
     data: AppendOnlyMappedFile,
     index: HashMap<u64, usize>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Recipe<'a> {
+    pub id: u64,
+    name: &'a str,
 }
 
 impl RecipeDatabase {
@@ -47,10 +52,14 @@ impl RecipeDatabase {
     }
 
     pub fn add(&mut self, recipe: &Recipe) -> Result<()> {
-        let id = recipe.id();
+        let id = recipe.id;
         let cur_offset = self.data.len();
 
-        self.data.append(recipe._tab.buf)?;
+        let recipe_bytes = serialize(recipe).map_err(|_| {
+            io::Error::new(io::ErrorKind::InvalidData, "Failed to serialize Recipe")
+        })?;
+
+        self.data.append(recipe_bytes.as_slice())?;
 
         // XXX I'm sure this can be better
         let mut buf = Vec::with_capacity(16);
@@ -67,10 +76,9 @@ impl RecipeDatabase {
     pub fn get(&self, recipe_id: u64) -> Option<Recipe> {
         self.index.get(&recipe_id).map_or(None, |offset| {
             // Care about read errors?
-            self.data
-                .from_offset(*offset)
-                .ok()
-                .map(|data| get_root_as_recipe(&data))
+            self.data.from_offset(*offset).ok().map(|data| {
+                deserialize(data).expect("Should be able to always deserialize written data")
+            })
         })
     }
 }
@@ -80,8 +88,6 @@ mod tests {
 
     use super::*;
     use tempfile;
-
-    use flatbuffers::FlatBufferBuilder;
 
     fn open_empty() -> RecipeDatabase {
         let tmpdir = tempfile::TempDir::new().unwrap();
@@ -98,48 +104,39 @@ mod tests {
         assert_eq!(None, open_empty().get(10));
     }
 
-    fn create_recipe<'a>(fbb: &'a mut FlatBufferBuilder, id: u64) -> Recipe<'a> {
-        fbb.reset();
-        let offset = Recipe::create(
-            fbb,
-            &RecipeArgs {
-                id: id,
-                ..Default::default()
-            },
-        );
-        fbb.finish(offset, None);
-        get_root_as_recipe(fbb.finished_data())
+    fn create_recipe<'a>(id: u64) -> Recipe<'a> {
+        Recipe {
+            id: id,
+            name: "caio",
+        }
     }
 
     #[test]
     fn can_add_and_get() {
         let mut db = open_empty();
-        let mut fbb = FlatBufferBuilder::new();
 
-        db.add(&create_recipe(&mut fbb, 1)).unwrap();
-        db.add(&create_recipe(&mut fbb, 2)).unwrap();
-        db.add(&create_recipe(&mut fbb, 3)).unwrap();
+        db.add(&create_recipe(1)).unwrap();
+        db.add(&create_recipe(2)).unwrap();
+        db.add(&create_recipe(3)).unwrap();
 
-        assert_eq!(1, db.get(1).unwrap().id());
-        assert_eq!(3, db.get(3).unwrap().id());
-        assert_eq!(2, db.get(2).unwrap().id());
+        assert_eq!(1, db.get(1).unwrap().id);
+        assert_eq!(3, db.get(3).unwrap().id);
+        assert_eq!(2, db.get(2).unwrap().id);
     }
 
     #[test]
     fn can_load_existing_database() {
         let tmpdir = tempfile::TempDir::new().unwrap();
         let db_path = tmpdir.path();
+        let mut db = RecipeDatabase::new(db_path).unwrap();
 
         {
-            let mut db = RecipeDatabase::new(db_path).unwrap();
-            let mut fbb = FlatBufferBuilder::new();
-
-            db.add(&create_recipe(&mut fbb, 1)).unwrap();
-            db.add(&create_recipe(&mut fbb, 2)).unwrap();
+            db.add(&create_recipe(1)).unwrap();
+            db.add(&create_recipe(2)).unwrap();
         }
 
         let existing_db = RecipeDatabase::new(db_path).unwrap();
-        assert_eq!(1, existing_db.get(1).unwrap().id());
-        assert_eq!(2, existing_db.get(2).unwrap().id());
+        assert_eq!(1, existing_db.get(1).unwrap().id);
+        assert_eq!(2, existing_db.get(2).unwrap().id);
     }
 }
