1ea56c0b945598e83116e3d40a7373ec6278c64a
Caio Romão <hello@caio.co>
2019-07-26 22:08:51 +0200

    Write directly instead of delegating
    This patch drops BytesDatabase and makes BincodeDatabase contain
    the whole functionality.

--- crates/tique/src/database/mod.rs
+++ crates/tique/src/database/mod.rs
@@ -1,8 +1,8 @@
 use std::io;
 
-mod mapped_file;
 mod database;
+mod mapped_file;
 
-pub use database::BytesDatabase;
+pub use database::{BincodeDatabase, Database};
 
 pub type Result<T> = io::Result<T>;
