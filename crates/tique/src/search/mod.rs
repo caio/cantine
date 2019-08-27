25328787689ca3d2c0e2c11fab912306a9b52d73
Caio Romão <hello@caio.co>
2019-08-27 14:47:47 +0200

    Add a specialized collector skeleton
    This patch starts exposing functionality that I'll use to collect
    documents from the index instead of tantivy's default TopDocs.
    
    There are two main reasons for this custom collector:
    
    1. I want stable results. Tantivy's collector only considers the
       used feature when scoring, which makes results seem "random" when
       many docs score the same (empty queries, pure negation, pure
       filtering - all features I want to provide).
    
    2. I want to play with marker-based results instead of plain pages
       because it gives:
    
       1. A better experience on incomplete results since I can say "give
          me the results after this item" instead of pulling the page
          and removing the items I saw before
    
       2. A direct relationship between memory consumption and number of
          results requested. Fancy offsetting may get there (with a 2n
          storage, I guess?), but currently all I can do is collect
          page_number * page_size then offset when rendering, which
          isn't a good idea when I allow _scanning_ a 1M+ sized index

--- crates/tique/src/search/mod.rs
+++ crates/tique/src/search/mod.rs
@@ -5,6 +5,7 @@ mod featureindex;
 mod featurevector;
 mod parser;
 mod query_parser;
+mod top_collector;
 
 type Result<T> = tantivy::Result<T>;
 
---
+++ crates/tique/src/search/top_collector.rs
@@ -0,0 +1,320 @@
+use std::{cmp::Ordering, collections::BinaryHeap};
+use tantivy::{DocAddress, DocId, Result, Score, SegmentLocalId, SegmentReader};
+
+/// This is pretty much tantivy's TopCollector and friends, tweaked to:
+///   * Have a stable ordering, using DocAddress to break even scores
+///   * Support pagination via a SearchMarker, without pages or offsets
+
+struct StableComparableDoc<D> {
+    feature: Score,
+    doc: D,
+}
+
+#[derive(Clone)]
+pub struct SearchMarker {
+    score: Score,
+    addr: DocAddress,
+}
+
+// A reference to the last picked result in a search result
+impl SearchMarker {
+    // Answers wether the previous search(es) would have listed
+    // a document with the given attributes.
+    pub fn has_seen(&self, feature: Score, segment_id: SegmentLocalId, doc_id: DocId) -> bool {
+        // If feature > self.score => yes
+        match self.score.partial_cmp(&feature) {
+            // Score is the same, so we should only pick the higher addresses
+            Some(Ordering::Equal) => DocAddress(segment_id, doc_id) <= self.addr,
+            // Given document has a score higher than the marker: it should have
+            // definitely appeared in the results
+            Some(Ordering::Less) => true,
+            // No document with lower score should appeared before the marker
+            _ => false,
+        }
+    }
+
+    pub fn new(score: Score, segment: SegmentLocalId, doc: DocId) -> Self {
+        SearchMarker {
+            score,
+            addr: DocAddress(segment, doc),
+        }
+    }
+}
+
+impl<D: PartialOrd> PartialOrd for StableComparableDoc<D> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<D: PartialOrd> Ord for StableComparableDoc<D> {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Highest feature first
+        match other.feature.partial_cmp(&self.feature) {
+            Some(Ordering::Equal) | None => {
+                // Break even by _lowest_ doc
+                self.doc.partial_cmp(&other.doc).unwrap_or(Ordering::Equal)
+            }
+            Some(rest) => rest,
+        }
+    }
+}
+
+impl<D: PartialOrd> PartialEq for StableComparableDoc<D> {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl<D: PartialOrd> Eq for StableComparableDoc<D> {}
+
+pub struct TopCollector {
+    limit: usize,
+    after: Option<SearchMarker>,
+}
+
+impl TopCollector {
+    /// Creates a top collector, with a number of documents equal to "limit".
+    ///
+    /// # Panics
+    /// The method panics if limit is 0
+    pub fn with_limit(limit: usize, after: Option<SearchMarker>) -> TopCollector {
+        if limit < 1 {
+            panic!("Limit must be strictly greater than 0.");
+        }
+        TopCollector { limit, after }
+    }
+
+    pub fn limit(&self) -> usize {
+        self.limit
+    }
+
+    pub fn merge_fruits(
+        &self,
+        children: Vec<Vec<(Score, DocAddress)>>,
+    ) -> Result<Vec<(Score, DocAddress)>> {
+        if self.limit == 0 {
+            return Ok(Vec::new());
+        }
+        let mut top_collector = BinaryHeap::new();
+        for child_fruit in children {
+            for (feature, doc) in child_fruit {
+                if top_collector.len() < self.limit {
+                    top_collector.push(StableComparableDoc { feature, doc });
+                } else if let Some(mut head) = top_collector.peek_mut() {
+                    if match head.feature.partial_cmp(&feature) {
+                        Some(Ordering::Equal) => doc < head.doc,
+                        Some(Ordering::Less) => true,
+                        _ => false,
+                    } {
+                        *head = StableComparableDoc { feature, doc };
+                    }
+                }
+            }
+        }
+        Ok(top_collector
+            .into_sorted_vec()
+            .into_iter()
+            .map(|cdoc| (cdoc.feature, cdoc.doc))
+            .collect())
+    }
+
+    pub fn for_segment<F: PartialOrd>(
+        &self,
+        segment_id: SegmentLocalId,
+        _: &SegmentReader,
+    ) -> Result<TopSegmentCollector> {
+        Ok(TopSegmentCollector::new(
+            segment_id,
+            self.limit,
+            self.after.clone(),
+        ))
+    }
+}
+
+pub struct TopSegmentCollector {
+    limit: usize,
+    heap: BinaryHeap<StableComparableDoc<DocId>>,
+    segment_id: u32,
+    after: Option<SearchMarker>,
+}
+
+impl TopSegmentCollector {
+    fn new(
+        segment_id: SegmentLocalId,
+        limit: usize,
+        after: Option<SearchMarker>,
+    ) -> TopSegmentCollector {
+        TopSegmentCollector {
+            limit,
+            heap: BinaryHeap::with_capacity(limit),
+            segment_id,
+            after,
+        }
+    }
+
+    pub fn harvest(self) -> Vec<(Score, DocAddress)> {
+        let segment_id = self.segment_id;
+        self.heap
+            .into_sorted_vec()
+            .into_iter()
+            .map(|comparable_doc| {
+                (
+                    comparable_doc.feature,
+                    DocAddress(segment_id, comparable_doc.doc),
+                )
+            })
+            .collect()
+    }
+
+    #[cfg(test)]
+    fn len(&self) -> usize {
+        self.heap.len()
+    }
+
+    #[inline(always)]
+    pub fn collect(&mut self, doc: DocId, feature: Score) {
+        if let Some(marker) = &self.after {
+            if marker.has_seen(feature, self.segment_id, doc) {
+                return;
+            }
+        }
+
+        if self.heap.len() >= self.limit {
+            if let Some(head) = self.heap.peek() {
+                if match head.feature.partial_cmp(&feature) {
+                    Some(Ordering::Equal) => doc < head.doc,
+                    Some(Ordering::Less) => true,
+                    _ => false,
+                } {
+                    if let Some(mut head) = self.heap.peek_mut() {
+                        head.feature = feature;
+                        head.doc = doc;
+                    }
+                }
+            }
+        } else {
+            self.heap.push(StableComparableDoc { feature, doc });
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{SearchMarker, TopSegmentCollector};
+    use tantivy::DocAddress;
+
+    #[test]
+    fn test_top_collector_not_at_capacity() {
+        let mut top_collector = TopSegmentCollector::new(0, 3, None);
+        top_collector.collect(1, 0.8);
+        top_collector.collect(3, 0.2);
+        top_collector.collect(5, 0.3);
+        assert_eq!(
+            top_collector.harvest(),
+            vec![
+                (0.8, DocAddress(0, 1)),
+                (0.3, DocAddress(0, 5)),
+                (0.2, DocAddress(0, 3))
+            ]
+        );
+    }
+
+    #[test]
+    fn test_top_collector_at_capacity() {
+        let mut top_collector = TopSegmentCollector::new(0, 4, None);
+        top_collector.collect(1, 0.8);
+        top_collector.collect(3, 0.2);
+        top_collector.collect(5, 0.3);
+        top_collector.collect(7, 0.9);
+        top_collector.collect(9, -0.2);
+        assert_eq!(
+            top_collector.harvest(),
+            vec![
+                (0.9, DocAddress(0, 7)),
+                (0.8, DocAddress(0, 1)),
+                (0.3, DocAddress(0, 5)),
+                (0.2, DocAddress(0, 3))
+            ]
+        );
+    }
+
+    #[test]
+    fn test_top_collector_stability() {
+        let mut top_collector = TopSegmentCollector::new(0, 5, None);
+        top_collector.collect(3, 0.1);
+        top_collector.collect(1, 0.1);
+        top_collector.collect(6, 0.1);
+        top_collector.collect(5, 0.5);
+        top_collector.collect(4, 0.5);
+        top_collector.collect(2, 0.1);
+        assert_eq!(
+            top_collector.harvest(),
+            vec![
+                (0.5, DocAddress(0, 4)),
+                (0.5, DocAddress(0, 5)),
+                (0.1, DocAddress(0, 1)),
+                (0.1, DocAddress(0, 2)),
+                (0.1, DocAddress(0, 3)),
+            ]
+        );
+    }
+
+    #[test]
+    fn search_marker() {
+        let same_score = 0.9;
+        let same_segment = 2;
+        let same_doc = 10;
+        let marker = SearchMarker::new(same_score, same_segment, same_doc);
+
+        // Higher score: seen
+        assert!(marker.has_seen(1.0, same_segment, same_doc));
+        // Lower score: not seen
+        assert_eq!(false, marker.has_seen(0.8, same_segment, same_doc));
+
+        // Same score, lower segment: seen
+        assert!(marker.has_seen(same_score, 0, same_doc));
+        // Same score, higher segment: not seen
+        assert_eq!(false, marker.has_seen(same_score, 3, same_doc));
+
+        // Same score and segment, lower doc: seen
+        assert!(marker.has_seen(same_score, same_segment, 9));
+        // Same score and segment, higher doc: not seen
+        assert_eq!(false, marker.has_seen(same_score, same_segment, 11));
+
+        // Exactly the same as After: seen
+        assert!(marker.has_seen(same_score, same_segment, same_doc));
+    }
+
+    #[test]
+    fn collection_with_a_marker_smoke() {
+        // Doc id=4 on segment=0 had score=0.5
+        let marker = SearchMarker::new(0.5, 0, 4);
+        let mut collector = TopSegmentCollector::new(0, 3, Some(marker));
+
+        // Every doc with a higher score has appeared already
+        collector.collect(7, 0.6);
+        collector.collect(5, 0.7);
+        assert_eq!(0, collector.len());
+
+        // Docs with the same score, but lower id too
+        collector.collect(3, 0.5);
+        collector.collect(2, 0.5);
+        assert_eq!(0, collector.len());
+
+        // And, of course, the same doc should not be collected
+        collector.collect(4, 0.5);
+        assert_eq!(0, collector.len());
+
+        // Lower scores are in
+        collector.collect(1, 0.0);
+        // Same score but higher doc, too
+        collector.collect(6, 0.5);
+
+        assert_eq!(
+            vec![(0.5, DocAddress(0, 6)), (0.0, DocAddress(0, 1))],
+            collector.harvest()
+        );
+    }
+}
