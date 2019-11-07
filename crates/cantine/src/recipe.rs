3b1fd97992b076cfa770ac76e6c6fbcf10ac9eeb
Caio Romão <hello@caio.co>
2019-11-07 21:19:01 +0100

    Start working on the filter model
    Design by "I Want to Play With A Feature" volume ??: proc_macro

--- crates/cantine/src/recipe.rs
+++ crates/cantine/src/recipe.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+use cantine_derive::FilterAndAggregation;
+
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct Recipe {
@@ -22,4 +24,57 @@ pub struct Recipe {
     pub carbohydrate_content: Option<f64>,
     pub protein_content: Option<f64>,
     pub similar_recipe_ids: Vec<u64>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, FilterAndAggregation)]
+pub struct Feature {
+    pub num_ingredients: u64,
+
+    pub prep_time: Option<u64>,
+    pub total_time: Option<u64>,
+    pub cook_time: Option<u64>,
+
+    pub calories: Option<f64>,
+    pub fat_content: Option<f64>,
+    pub carbohydrate_content: Option<f64>,
+    pub protein_content: Option<f64>,
+
+    pub diet_lowcarb: Option<f64>,
+    pub diet_vegetarian: Option<f64>,
+    pub diet_vegan: Option<f64>,
+    pub diet_keto: Option<f64>,
+    pub diet_paleo: Option<f64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Sort {
+    Relevance,
+    NumIngredients,
+    TotalTime,
+    CookTime,
+    PrepTime,
+    Calories,
+    FatContent,
+    CarbContent,
+    ProteinContent,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct SearchQuery {
+    fulltext: Option<String>,
+    sort: Option<Sort>,
+    num_items: Option<u8>,
+    filters: Option<FeatureFilterQuery>,
+    agg: Option<FeatureAggregationQuery>,
+    // TODO decide how to expose After<score,@id>
+    after: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct SearchResult {
+    // TODO RecipeCard
+    items: Vec<u64>,
+    agg: Option<FeatureAggregationResult>,
+    // TODO Ref=SearchQuery.after
+    after: Option<String>,
 }
---
+++ crates/cantine_derive/src/lib.rs
@@ -0,0 +1,102 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{format_ident, quote, quote_spanned};
+use syn::{
+    parse_macro_input, spanned::Spanned, Data, DeriveInput, Field, Fields, GenericArgument, Ident,
+    PathArguments, Type, Visibility,
+};
+
+#[proc_macro_derive(FilterAndAggregation)]
+pub fn derive_filter_and_agg(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let filter_query = make_struct(&input.data, &input.ident, "FilterQuery", make_option_range);
+
+    let agg_query = make_struct(
+        &input.data,
+        &input.ident,
+        "AggregationQuery",
+        make_vec_range,
+    );
+
+    let agg_result = make_struct(&input.data, &input.ident, "AggregationResult", make_vec);
+
+    TokenStream::from(quote! {
+        #filter_query
+        #agg_query
+        #agg_result
+    })
+}
+
+fn make_option_range(field: &Field) -> TokenStream2 {
+    let name = &field.ident;
+    let ty = extract_type_if_option(&field.ty).unwrap_or(&field.ty);
+
+    quote_spanned! { field.span()=> pub #name: Option<std::ops::Range<#ty>> }
+}
+
+fn make_vec(field: &Field) -> TokenStream2 {
+    let name = &field.ident;
+    let ty = extract_type_if_option(&field.ty).unwrap_or(&field.ty);
+    quote_spanned! { field.span()=> pub #name: Vec<#ty> }
+}
+
+fn make_vec_range(field: &Field) -> TokenStream2 {
+    let name = &field.ident;
+    let ty = extract_type_if_option(&field.ty).unwrap_or(&field.ty);
+    quote_spanned! { field.span()=> pub #name: Vec<std::ops::Range<#ty>> }
+}
+
+fn make_struct(
+    data: &Data,
+    base: &Ident,
+    name: &str,
+    f: fn(&Field) -> TokenStream2,
+) -> TokenStream2 {
+    let struct_name = format_ident!("{}{}", base, name);
+    match data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => {
+                let struct_fields = fields
+                    .named
+                    .iter()
+                    .filter(|field| match &field.vis {
+                        Visibility::Public(_) => true,
+                        _ => false,
+                    })
+                    .map(|field| {
+                        let tokens = f(&field);
+                        quote_spanned! {
+                            field.span()=> #tokens
+                        }
+                    });
+                quote! {
+                    #[derive(serde::Serialize, serde::Deserialize, Default, Debug)]
+                    pub struct #struct_name {
+                        #(#struct_fields),*
+                    }
+                }
+            }
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
+}
+
+fn extract_type_if_option(ty: &Type) -> Option<&Type> {
+    if let Type::Path(tp) = ty {
+        if tp.path.segments.len() == 1 && tp.path.segments.first().unwrap().ident == "Option" {
+            if let Some(type_params) = tp.path.segments.first() {
+                if let PathArguments::AngleBracketed(ref params) = type_params.arguments {
+                    let generic_arg = params.args.first().unwrap();
+                    if let GenericArgument::Type(ty) = generic_arg {
+                        return Some(ty);
+                    }
+                }
+            }
+        }
+    }
+    None
+}
