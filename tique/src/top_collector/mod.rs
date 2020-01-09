1c1617a246ceeef6853aef672fa7de58898e07c6
Caio Romão <hello@caio.co>
2020-01-09 19:10:53 +0100

    Add support for (reversible) fast field scoring
    This patch replaces the original `ordered_by_XXX_fast_field` with a more
    explicit `fastfield::{ascending,descending}` idiom. Essentially, this is
    a very verbose way of encoding a runtime boolean check into the type
    system.

--- tique/src/top_collector/mod.rs
+++ tique/src/top_collector/mod.rs
@@ -1,12 +1,12 @@
 mod conditional_collector;
 mod custom_score;
-mod field;
 mod topk;
+
+pub mod fastfield;
 
 pub use conditional_collector::{
     CheckCondition, CollectionResult, ConditionForSegment, ConditionalTopCollector,
     ConditionalTopSegmentCollector, SearchMarker,
 };
-pub use custom_score::CustomScoreTopCollector;
-pub use field::{ordered_by_f64_fast_field, ordered_by_i64_fast_field, ordered_by_u64_fast_field};
+pub use custom_score::{CustomScoreTopCollector, DocScorer, ScorerForSegment};
 pub use topk::{Scored, TopK};
