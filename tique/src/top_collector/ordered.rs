654b137fd7c862ba86a46ebb80dc879753f015b4
Caio Romão <hello@caio.co>
2020-01-18 15:13:28 +0100

    OrderedCollector: Support Descending ordering
    There we go, a fully working conditional top collector. Now it's a
    matter of outlining the {Custom,Tweaked}Collector code and we're
    gold.

--- tique/src/top_collector/ordered.rs
+++ tique/src/top_collector/ordered.rs
@@ -94,7 +94,6 @@ where
     type Fruit = CollectionResult<Score>;
 
     fn collect(&mut self, doc: DocId, score: Score) {
-        self.topk.visit(score, doc);
         self.total += 1;
         if self.condition.check(self.segment_id, doc, score) {
             self.visited += 1;
@@ -132,7 +131,6 @@ mod topk {
     };
 
     use super::{CollectionResult, DocId, Scored, TopK, TopKProvider};
-    // use crate::top_collector::Scored;
 
     pub struct Ascending;
 
@@ -176,15 +174,57 @@ mod topk {
         }
     }
 
+    pub struct Descending;
+
+    impl<T: PartialOrd> TopKProvider<T> for Descending {
+        type TK = DescendingTopK<T, DocId>;
+
+        fn new_topk(limit: usize) -> Self::TK {
+            DescendingTopK {
+                limit,
+                heap: BinaryHeap::with_capacity(limit),
+            }
+        }
+
+        fn merge_many(limit: usize, items: Vec<CollectionResult<T>>) -> CollectionResult<T> {
+            let mut topk = DescendingTopK {
+                limit,
+                heap: BinaryHeap::with_capacity(limit),
+            };
+
+            let mut total = 0;
+            let mut visited = 0;
+
+            for item in items {
+                total += item.total;
+                visited += item.visited;
+
+                for collected in item.items {
+                    topk.visit(collected.score, collected.doc);
+                }
+            }
+
+            CollectionResult {
+                total,
+                visited,
+                items: topk
+                    .into_sorted_vec()
+                    .into_iter()
+                    .map(|(score, doc)| Scored { score, doc })
+                    .collect(),
+            }
+        }
+    }
+
     pub struct AscendingTopK<S, D> {
         limit: usize,
         heap: BinaryHeap<Scored<S, D>>,
     }
 
-    //     pub struct DescendingTopK<S, D> {
-    //         limit: usize,
-    //         heap: BinaryHeap<Scored<S, D>>,
-    //     }
+    pub struct DescendingTopK<S, D> {
+        limit: usize,
+        heap: BinaryHeap<Reverse<Scored<S, D>>>,
+    }
 
     impl<T: PartialOrd, D: PartialOrd> AscendingTopK<T, D> {
         fn visit(&mut self, score: T, doc: D) {
@@ -206,7 +246,7 @@ mod topk {
             self.heap
                 .into_sorted_vec()
                 .into_iter()
-                .map(|Scored { score, doc }| (score, doc))
+                .map(|s| (s.score, s.doc))
                 .collect()
         }
 
@@ -214,7 +254,40 @@ mod topk {
             self.heap
                 .into_vec()
                 .into_iter()
-                .map(|Scored { score, doc }| (score, doc))
+                .map(|s| (s.score, s.doc))
+                .collect()
+        }
+    }
+
+    impl<T: PartialOrd, D: PartialOrd> DescendingTopK<T, D> {
+        fn visit(&mut self, score: T, doc: D) {
+            if self.heap.len() < self.limit {
+                self.heap.push(Reverse(Scored { score, doc }));
+            } else if let Some(mut head) = self.heap.peek_mut() {
+                if match head.0.score.partial_cmp(&score) {
+                    Some(Ordering::Equal) => doc < head.0.doc,
+                    Some(Ordering::Less) => true,
+                    _ => false,
+                } {
+                    head.0.score = score;
+                    head.0.doc = doc;
+                }
+            }
+        }
+
+        fn into_sorted_vec(self) -> Vec<(T, D)> {
+            self.heap
+                .into_sorted_vec()
+                .into_iter()
+                .map(|s| (s.0.score, s.0.doc))
+                .collect()
+        }
+
+        fn into_vec(self) -> Vec<(T, D)> {
+            self.heap
+                .into_vec()
+                .into_iter()
+                .map(|s| (s.0.score, s.0.doc))
                 .collect()
         }
     }
@@ -232,6 +305,20 @@ mod topk {
             AscendingTopK::into_vec(self)
         }
     }
+
+    impl<T: PartialOrd> TopK<T, DocId> for DescendingTopK<T, DocId> {
+        fn visit(&mut self, score: T, doc: DocId) {
+            DescendingTopK::visit(self, score, doc);
+        }
+
+        fn into_sorted_vec(self) -> Vec<(T, DocId)> {
+            DescendingTopK::into_sorted_vec(self)
+        }
+
+        fn into_vec(self) -> Vec<(T, DocId)> {
+            DescendingTopK::into_vec(self)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -239,8 +326,70 @@ mod tests {
 
     use super::*;
 
+    use tantivy::{query::TermQuery, schema, Document, Index, Result, Term};
+
     #[test]
-    fn pass() {
-        let _collector = OrderedCollector::<Score, topk::Ascending, _>::with_limit(10, true);
+    fn collect_even_public_ids_ascendingly() -> Result<()> {
+        let mut builder = schema::SchemaBuilder::new();
+
+        let text_field = builder.add_text_field("text", schema::TEXT);
+
+        let index = Index::create_in_ram(builder.build());
+        let mut writer = index.writer_with_num_threads(1, 3_000_000)?;
+
+        let add_doc = |text: &str| {
+            let mut doc = Document::new();
+            doc.add_text(text_field, text);
+            writer.add_document(doc);
+        };
+
+        const NUM_DOCS: usize = 3;
+        add_doc("the first doc is simple");
+        add_doc("the second doc is a bit larger");
+        add_doc("and the third document is rubbish");
+
+        writer.commit()?;
+
+        let reader = index.reader()?;
+        let searcher = reader.searcher();
+
+        let collector_asc = OrderedCollector::<_, topk::Ascending, _>::with_limit(NUM_DOCS, true);
+        let collector_desc = OrderedCollector::<_, topk::Descending, _>::with_limit(NUM_DOCS, true);
+
+        // Query for "the", which matches all docs and yields
+        // a distinct score for each
+        let query = TermQuery::new(
+            Term::from_field_text(text_field, "the"),
+            schema::IndexRecordOption::WithFreqsAndPositions,
+        );
+        let (asc, desc) = searcher.search(&query, &(collector_asc, collector_desc))?;
+
+        assert_eq!(NUM_DOCS, asc.items.len());
+        assert_eq!(NUM_DOCS, desc.items.len());
+
+        let asc_scores = asc
+            .items
+            .iter()
+            .map(|scored| scored.score)
+            .collect::<Vec<_>>();
+
+        let mut prev = None;
+        for score in &asc_scores {
+            if let Some(previous) = prev {
+                assert!(previous < score, "The scores should be ascending");
+            }
+            prev = Some(score)
+        }
+
+        let mut desc_scores = desc
+            .items
+            .iter()
+            .map(|scored| scored.score)
+            .collect::<Vec<_>>();
+
+        desc_scores.reverse();
+        assert_eq!(asc_scores, desc_scores);
+
+        Ok(())
     }
 }
