e965be8958430498da25a9aab973bcc777f37cc5
Caio Romão <hello@caio.co>
2020-01-25 09:40:15 +0100

    Split compilation features into default and unstable

--- tique/src/lib.rs
+++ tique/src/lib.rs
@@ -1,7 +1,13 @@
 pub mod conditional_collector;
+
+#[cfg(feature = "unstable")]
 pub mod queryparser;
 
+#[cfg(feature = "unstable")]
 mod derive;
 
+#[cfg(feature = "unstable")]
 pub use derive::RangeStats;
+
+#[cfg(feature = "unstable")]
 pub use tique_derive::FilterAndAggregation;
